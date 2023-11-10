028e1f68e2b36945c4d910bce8a4b9d5a90843ae
Zerumi <46845708+Zerumi@users.noreply.github.com>
2023-11-11 02:25:43 +0300

    chore: removed unnecessary files

--- c/ch13_10/solution/include/error_handler.h
+++ c/ch13_10/solution/include/error_handler.h
@@ -2,13 +2,17 @@
 #define IMAGE_TRANSFORMER_ERROR_HANDLER_H
 
 #include "image_bmp_worker.h"
-#include "io_handler.h"
 
 #include <errno.h>
 #include <stdbool.h>
 #include <string.h>
 
 void print_error(const char* error);
+
+enum file_status {
+    FILE_OK = 0,
+    FILE_ERROR
+};
 
 bool console_handle_read_error(enum bmp_read_status read_status);
 bool console_handle_write_error(enum bmp_write_status write_status);
--- c/ch13_10/solution/src/io_handler.c
+++
@@ -1,7 +0,0 @@
-#include "io_handler.h"
-
-enum file_status try_open_file(const char* filename, const char* mode, FILE** out) {
-    *out = fopen(filename, mode);
-    if (!*out) return FILE_ERROR;
-    return FILE_OK;
-}
